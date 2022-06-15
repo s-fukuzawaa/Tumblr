56fb6fdd218da15d7f7eb7dabcdd32e18ae6a57b
Airei Fukuzawa <eyeray@fb.com>
2022-06-15 09:34:21 -0700

    Initial Commit

---
+++ Tumblr/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Tumblr
+//
+//  Created by Airei Fukuzawa on 6/15/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Tumblr/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Tumblr
+//
+//  Created by Airei Fukuzawa on 6/15/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
